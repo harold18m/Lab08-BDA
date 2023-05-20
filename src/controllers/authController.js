08225d411de29065cd542f6a56ae49f8aca3b72c
Harold Medrano <118697566+harold18m@users.noreply.github.com>
2023-05-19 22:32:54 -0500

    Update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,62 +7,41 @@ const verifyToken = require('./verifyToken');
 
 const User = require('../models/User');
 
-router.post('/signup', async (req, res, next) => {
+router.post('/register', async (req, res) => {
     const { username, email, password } = req.body;
-    const user = new User(
-       {
-          username,
-          email,
-          password
-       } 
-    );
+    const user = new User({ username, email, password });
 
-    //console.log(username, email, password)
-    //console.log(user)
-    user.password = await user.encryptPassword(user.password)
+    user.password = await user.encryptPassword(user.password);
     await user.save();
 
-    const token = jwt.sign({id: user._id}, config.secret, {
-        expiresIn: 60 * 60 * 24
-    })
-    //res.json({message: 'Received'})
-    res.json({auth: true, token})
-})
+    const token = jwt.sign({ id: user._id }, config.secret, { expiresIn: 60 * 60 * 24 });
+    return res.send(`User registered. Token: ${token}`);
+});
 
-router.get('/me', verifyToken, async (req, res, next) => {
+router.get('/me', verifyToken, async (req, res) => {
     const user = await User.findById(req.userId, { password: 0 });
-    if(!user){
-        return res.status(404).send('No user found....!!!');
-    }
+    if (!user) return res.status(404).send('No user found.');
 
     res.json(user);
-})
+});
 
-router.post('/signin', async (req, res, next) => {
-    const { email, password } = req.body; 
-    //console.log(email, password);
-    const user = await User.findOne({email: email})
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-    if(!user){
-        return res.status(404).send("The user doesn't exists");
-    }
+    if (!user) return res.status(404).send("User doesn't exist.");
 
     const validPassword = await user.validatePassword(password);
-    //console.log(passwordIsValid);
-    if(!validPassword){
-        return res.status(401).json({auth: false, token: null});
-    }
+    //if (!validPassword) return res.status(401).json({ auth: false, token: null });
+    if (!validPassword) return res.status(401).send("Wrong password or email.");
 
-    const token = jwt.sign({id: user._id}, config.secret, {
-        expiresIn: 60 * 60 * 24
-    });
-    
-    res.json({auth: true, token});
-})
+    const token = jwt.sign({ id: user._id }, config.secret, { expiresIn: 60 * 60 * 24 });
+    //utilizamos el metodo render para mostrar la pagina dashboard
+    return res.render('dashboard', { token }); 
+});
 
-router.get('/dashboard', verifyToken, (req, res, next) => {
-    res.json('dashboard');
-})
+router.get('/dashboard', verifyToken, (req, res) => {
+    res.json('dashboard'); 
+});
 
-
-module.exports = router
+module.exports = router;
